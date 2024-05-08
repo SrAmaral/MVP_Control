1c1a68fb9620bd49c9e5dda5a384f6dae570d513
wjPbeanni <pedro.lemos@webjump.com.br>
2024-05-08 00:39:10 -0300

    feature: add spring starter

---
+++ java_image/src/main/java/com/dupeapps/ganoncontrol/GanoncontrolApplication.java
@@ -0,0 +1,13 @@
+package com.dupeapps.ganoncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanoncontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanoncontrolApplication.class, args);
+	}
+
+}
