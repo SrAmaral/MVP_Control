ef3ee4f99add4150915a8d0e44e5c3bfd3243274
wjPbeanni <pedro.lemos@webjump.com.br>
2024-05-08 01:49:01 -0300

    feature: add basic client and user crud operations

---
+++ java_image/src/main/java/com/dupeapps/ganoncontrol/accounts/client/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.dupeapps.ganoncontrol.accounts.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
