4207cf0a53b387b42137f3264040560677f7ff54
Pbeanni <pedroallanbfl@gmail.com>
2024-09-26 19:03:54 -0300

    fix color values on not_found pages moved navbar from pages to layout

---
+++ src/app/ui/components/topbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+} from "./breadcrumb";
+import { ContentLayout } from "./content-layout";
+import PlaceholderContent from "./placeholder-content";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { findActiveLabel, getMenuList } from "../lib/menu-list";
+
+interface LayoutProps {
+  children?: React.ReactNode;
+  title?: string;
+}
+
+export default function TopBar({ children }: LayoutProps) {
+  const pathname = usePathname();
+  const label = findActiveLabel(pathname) || "";
+  const pathSegments = pathname.split("/").filter(Boolean);
+  const UpFirstLetter = (slug: string) => {
+    return slug
+      .replace(/-/g, " ")
+      .replace(/\b\w/g, (char) => char.toUpperCase());
+  };
+
+  return (
+    <ContentLayout title={label}>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+
+          {pathSegments.map((segment, index) => (
+            <React.Fragment key={index}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href={`/${pathSegments.slice(0, index + 1).join("/")}`}>
+                    {UpFirstLetter(segment)}
+                  </Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <PlaceholderContent>
+        <div className="h-full w-full">{children}</div>
+      </PlaceholderContent>
+    </ContentLayout>
+  );
+}
