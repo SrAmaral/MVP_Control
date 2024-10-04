9ffe04f3ca6aeb1f4bca5ac365839a0b1bd4b471
thiago5171 <tgazaroli@gmail.com>
2024-10-03 21:55:09 -0300

    refactor: Update user and client list pages
    - Refactor the user list page to import the updated ColumnUser component from the correct path.
    - Create a new client list page and import the ColumnClient component.
    - Add the DataTable component to both pages and fetch data using the respective API calls.
    
    Closes #32

--- src/app/(modules)/clients/(pages)/list/page.tsx
+++ src/app/(modules)/clients/(pages)/list/page.tsx
@@ -1,3 +1,13 @@
+ 
+import { DataTable } from "~/components/ui/dataTable";
+import { api } from "~/core/trpc/callers/react"; 
+import ColumnClient from "../../utils/columns/Client";
+
 export default function Page() {
-  return <h1>List Clients</h1>;
-}
+  const users = api.clients.listClient.useQuery();
+ 
+
+    return (
+      <DataTable columns={ColumnClient()} data={users.data ?? []} loading={users.isLoading} />
+    );
+  }
---
+++ src/app/(modules)/clients/utils/columns/Client.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { CaretSortIcon, InfoCircledIcon, Pencil1Icon, Pencil2Icon, TrashIcon } from "@radix-ui/react-icons";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "~/components/ui/button";
+import {  Client } from "@prisma/client";
+ 
+interface columnUserProps {
+  erase?: (id: string) => void;
+  edit?: (id: string) => void;
+  info?: (id: string) => void;
+}
+
+function ColumnClient(
+  { erase, edit, info }: columnUserProps = {}  
+): ColumnDef<Client>[] {
+  const columns: ColumnDef<Client>[] = [
+    {
+      accessorKey: "fantasyName",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                className="text-left"
+            >
+                Nome Fantasia
+                <CaretSortIcon className="ml-2 h-4 w-4" />
+            </Button>
+            ),
+        cell: ({ row }) => (
+            <div className="text-left">{row.original.fantasyName}</div>
+        ),
+    },
+    {
+      accessorKey: "cnpj",
+      header: ({ column }) => (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="text-left"
+        >
+          CNPJ
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: ({ row }) => (
+        <div className="text-left">{row.original.cnpj}</div>
+      ),
+    },
+    {
+      accessorKey: "Ações",
+      enableHiding: true,
+      header: () => (
+        <div className="text-center">Ações</div>
+      ),
+      cell: ({ row }) => (
+        <div className="flex justify-between space-x-2">
+          <TrashIcon className="h-5 w-5 cursor-pointer" onClick={() => erase?.(row.original.id)} />
+          <Pencil2Icon className="h-5 w-5 cursor-pointer" onClick={() => edit?.(row.original.id)} />
+          <InfoCircledIcon className="h-5 w-5 cursor-pointer" onClick={() => info?.(row.original.id)} />
+        </div>
+      ),
+    },
+  ];
+
+  return columns;
+}
+
+export default ColumnClient;
