d5d017923569daebd9d5a3a4cd68dc2e07836a10
thiago5171 <tgazaroli@gmail.com>
2024-10-08 21:06:05 -0300

    correct call ontoast

--- src/app/(modules)/clients/(pages)/list/[clientId]/page.tsx
+++ src/app/(modules)/clients/(pages)/list/[clientId]/page.tsx
@@ -1,33 +1,34 @@
 "use client";
+
 import { useParams, useRouter } from "next/navigation";
 import ClientsFormCreate from "~/components/Clients/form";
 import { api } from "~/core/trpc/callers/react";
 import { ClientType } from "../../../module/types";
 import LoadingSpinner from "~/components/ui/loading";
 import { useToast } from "~/hooks/use-toast";
-import { Button } from "~/components/ui/button";
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 
 export default function Page() {
   const { clientId } = useParams();
-  const { toast } = useToast()
-  const { data: client, isLoading, isError, error  } = api.clients.listClientById.useQuery(clientId as string);
-  const router =  useRouter()
+  const router = useRouter();
+  const { toast } = useToast();
 
-  if (!client || isError) {
-    toast({
-      title: "Cliente não encontrado ou não existe",
-      variant: "error",
-    });
+  const { data: client, isLoading, isError } = api.clients.listClientById.useQuery(clientId as string);
+  
+  useEffect(() => {
+    if (!isLoading && (isError || !client)) {
+      toast({
+        title: "Cliente não encontrado ou não existe",
+        variant: "error",
+      });
 
-    router.push("/clients/list");
-  }
+      router.push("/clients/list");
+    }
+  }, [isLoading, isError, client, toast, router]);
 
   if (isLoading) {
     return <LoadingSpinner className="h-[calc(100vh-70px)]" />;
   }
 
-  return (
-  <ClientsFormCreate client={client as ClientType} />
-  );
+  return <ClientsFormCreate client={client as ClientType} />;
 }
