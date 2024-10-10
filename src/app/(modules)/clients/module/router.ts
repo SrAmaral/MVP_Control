aee51786a8e5a36594dc0d75e14c348f22b6ce2e
Eduardo Amaral <edu.amaral8@gmail.com>
2024-10-10 15:08:11 -0300

    Resolve comments qa

--- src/app/(modules)/clients/module/router.ts
+++ src/app/(modules)/clients/module/router.ts
@@ -1,5 +1,34 @@
-import { createTRPCRouter } from "~/core/trpc/trpc";
-import { createClient, deleteClient, listClient, listClientById, updateClient } from "./procedures/procedures";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/core/trpc/trpc";
+import { clientService } from "./service";
+import { clientSchema } from "./types";
+
+export const createClient = publicProcedure
+  .input(
+    clientSchema
+  )
+  .mutation(({ctx: {clientService}, input}) => {
+    return clientService.ClientCreate(input)
+  })
+
+export const listClient = publicProcedure
+  .query(({ ctx: {clientService}}) => {
+    return clientService.ListCLients()
+  })
+
+  export const listClientById = publicProcedure
+  .input(z.string())
+  .query(({ ctx: {clientService},input}) => {
+    return clientService.ListClientById(input)
+  })
+
+export const updateClient = publicProcedure.input(clientSchema).mutation(({ctx:{db},input}) => {
+    return clientService.updateCLient(input,db)
+})
+
+export const deleteClient = publicProcedure.input(z.string()).mutation(({ctx:{db},input}) => {
+    return clientService.deleteClient(input,db)
+})
 
 
 export const clientsRouter = createTRPCRouter({
