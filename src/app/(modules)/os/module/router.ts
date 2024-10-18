0ea57cb48fe2da4115d86deaa73ec4764e9f615c
Eduardo Amaral <edu.amaral8@gmail.com>
2024-10-17 21:24:29 -0300

    crete a os services

--- src/app/(modules)/os/module/router.ts
+++ src/app/(modules)/os/module/router.ts
@@ -1,6 +1,37 @@
-import { createTRPCRouter } from "~/core/trpc/trpc";
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/core/trpc/trpc";
+import { clientService, osService } from "./service";
+import { clientSchema, osSchema } from "./types";
+
+export const createOs = publicProcedure
+  .input(
+    osSchema
+  )
+  .mutation(({ctx: {db}, input}) => {
+    return osService.OsCreate(input, db)
+  })
+
+export const listClient = publicProcedure
+  .query(({ ctx: {clientService}}) => {
+    return clientService.ListCLients()
+  })
+
+  export const listClientById = publicProcedure
+  .input(z.string())
+  .query(({ ctx: {clientService},input}) => {
+    return clientService.ListClientById(input)
+  })
+
+export const updateClient = publicProcedure.input(clientSchema).mutation(({ctx:{db},input}) => {
+    return clientService.updateCLient(input,db)
+})
+
+export const deleteClient = publicProcedure.input(z.string()).mutation(({ctx:{db},input}) => {
+    return clientService.deleteClient(input,db)
+})
 
 
 export const osRouter = createTRPCRouter({
     
-});
+  });
+  
--- src/app/(modules)/os/module/service.ts
+++ src/app/(modules)/os/module/service.ts
@@ -1,4 +1,115 @@
+import { type PrismaClient } from "@prisma/client";
+import { type OSType } from "./types";
 
+export const OsCreate = async (data: OSType, db: PrismaClient) => {
+  const {client, users , ...osData } = data;
+
+  const os = await db.oS.create({
+    data: {
+      ...osData,
+      ...(client && {
+        client: {
+          connect: {
+            id: client.id ?? ""
+          }
+        }
+      }),
+      ...(users && {
+        users: {
+          connect: users.map((user) => ({
+            id: user.id ?? ""
+          }))
+        }
+      })
+    }
+  });
+  
+  return os;
+}
+
+export const OsList = async (db: PrismaClient) => {
+  try {
+    return await db.oS.findMany({
+      where: {
+        logicalDeleted: false
+      },
+      include: {
+        client: true,
+        users: true
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const OsListById = async (id: string, db: PrismaClient) => {
+  try {
+    return await db.oS.findUnique({
+      where: {
+        id, AND: {
+          logicalDeleted: false
+        }
+      },
+      include: {
+        client: true,
+        users: true
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+export const OsUpdate = async (id: string, data: OSType, db: PrismaClient) => {
+  const {client, users , ...osData } = data;
+
+  const os = await db.oS.update({
+    where: {
+      id
+    },
+    data: {
+      ...osData,
+      ...(client && {
+        client: {
+          connect: {
+            id: client.id ?? ""
+          }
+        }
+      }),
+      ...(users && {
+        users: {
+          connect: users.map((user) => ({
+            id: user.id ?? ""
+          }))
+        }
+      })
+    }
+  });
+  
+  return os;
+}
+
+export const OsDelete = async (id: string, db: PrismaClient) => {
+  try {
+    return await db.oS.update({
+      where: {
+        id
+      },
+      data: {
+        logicalDeleted: true
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export const osService = {
-};
+  OsCreate,
+  OsList,
+  OsListById,
+  OsUpdate,
+  OsDelete
+  };
