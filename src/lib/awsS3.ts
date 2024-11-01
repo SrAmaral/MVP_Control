2bd8c0500f69ad86af803f010f4b217590dbbd78
Eduardo Amaral <edu.amaral8@gmail.com>
2024-11-01 19:34:07 -0300

    env

--- src/lib/awsS3.ts
+++ src/lib/awsS3.ts
@@ -5,7 +5,7 @@ const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
   },
-  region: process.env.AWS_REGION,
+  // region: process.env.AWS_REGION,
 });
 
 export default s3;
