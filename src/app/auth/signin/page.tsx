9b0d18264c0457adac7bb1eae231924e46029c47
Eduardo Amaral <edu.amaral8@gmail.com>
2024-11-12 11:27:00 -0300

    add login

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Suspense } from "react";
 import LoginFormComponent from "~/components/Login/loginComponent";
 
 export default function SigninPage() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-300">
-      <LoginFormComponent />
-    </div>
+    <Suspense>
+      <div className="flex h-screen items-center justify-center bg-gray-300">
+        <LoginFormComponent />
+      </div>
+    </Suspense>
   );
 }
